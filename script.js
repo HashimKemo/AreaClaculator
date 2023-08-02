40d1807d339bc1bcd63fb182154502f10510f571
HashimKamal <hashimskips1.123@gmail.com>
2023-08-02 09:11:48 +0200

    the mainframe

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const width = document.getElementById('width');
 const height = document.getElementById('height');
 const area = document.getElementById('area');
 const enter = document.querySelectorAll(".in");
+let li = document.getElementsByTagName("li");
 
 function getArea (){
-  area.textContent= 'Area: '+ width.value*height.value
+  li0 =document.createElement("li");
+  li1 =document.createElement("li");
+  li2 =document.createElement("li");
+  li0.appendChild(document.createTextNode('المساحة: '+ width.value*height.value+" م ² "))
+  let cArea = ((width.value*height.value)/4200).toPrecision(6);
+  li1.appendChild(document.createTextNode(' المساحة بالفدان: '+cArea +" فدان "))
+  let hArea = ((width.value*height.value)/10000);
+  li2.appendChild(document.createTextNode(' المساحة بالهكتار: '+hArea +" هكتار "))
+  ul.appendChild(li0)
+  ul.appendChild(li1)
+  ul.appendChild(li2)
 }
 
 calc.addEventListener("click",function Area(){
@@ -15,9 +26,11 @@ calc.addEventListener("click",function Area(){
   }
 })
 
-enter[1].addEventListener("input", function EArea (){
-  if (width.value >0 && height.value > 0){
+enter[1].addEventListener("keypress", function EArea (e){
+  if (width.value >0 && height.value > 0 && e.which ===13){
     getArea()
+    width.value=""
+    height.value=""
   }
 })
 //Area--------
@@ -29,7 +42,7 @@ const converted = document.getElementById("converter");
 const ConResult = document.getElementById("ConResult")
 
 function convert (){
-  let cArea = converted.value/4046.8564224;
+  // let cArea = converted.value/4046.8564224;
   ConResult.textContent="Acr: "+cArea.toPrecision(5)
 }
 
